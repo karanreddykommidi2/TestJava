bb4cc9ea409a38e99578dfbcea941c21af41dd48
Srinivasarao Dumpala <srinivasarao.dumpala@cgi.com@cgi.com>
2018-02-12 02:30:38 -0500

    Signed-off-by: Srinivasarao Dumpala <srinivasarao.dumpala@cgi.com@cgi.com>

---
+++ ReadFile/src/GreetMain.java
@@ -0,0 +1,10 @@
+
+public class GreetMain extends Greetings{
+
+	public static void main(String[] args) {
+		Greetings g = new Greetings();
+				System.out.println(g.Greet("Jenkins"));
+
+	}
+
+}
---
+++ ReadFile/src/Greetings.java
@@ -0,0 +1,7 @@
+
+public class Greetings {
+	
+     public String Greet(String messagae ) {
+    	 return "Hello I am "+ messagae;
+}
+}
